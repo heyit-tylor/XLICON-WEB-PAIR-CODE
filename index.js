5964c10157708f81872ce0cb3ce39323c9faa91d
heyit-tylor <163938073+heyit-tylor@users.noreply.github.com>
2024-10-08 17:47:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ res.sendFile(__path + '/main.html')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
-    console.log(`YoutTube: S4 Salman YT\nTelegram: ahmmitech\nGitHub: @salmanytofficial\nInstsgram: ahmmikun\n\nServer running on http://localhost:` + PORT)
+    console.log(`YoutTube: heyits_tylor\nTelegram: heyits_tylor\nGitHub: heyit-tylor\nInstsgram: heyits_tylor\n\nServer running on http://localhost:` + PORT)
 })
 
 module.exports = app
