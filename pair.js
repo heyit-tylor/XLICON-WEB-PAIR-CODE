58acf4cc983e6fcc6ec93ccd1f603249d2d5215b
heyit-tylor <163938073+heyit-tylor@users.noreply.github.com>
2024-10-08 17:56:41 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -58,7 +58,7 @@ router.get('/', async (req, res) => {
                 }, {
                     quoted: xeonses
                 });
-				await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `*_🛑Do not share this file with anybody_*\n\n© *_Subscribe_* www.youtube.com/@s4salmanyt *_on Youtube_*` }, {quoted: xeonses});
+				await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `*_🛑Do not share this file with anybody_*\n\n© *_Subscribe_* www.youtube.com/@heyits_tylor *_on Youtube_*` }, {quoted: xeonses});
         await delay(100);
         return await removeFile('./session');
         process.exit(0)
